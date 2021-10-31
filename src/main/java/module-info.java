390c34805a1e9ab4e2666b35757cf19534c69f3b
Shamayko <shamayko@gmail.com>
2021-10-31 21:51:47 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.geekbrainscloudstorage {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.example.geekbrainscloudstorage to javafx.fxml;
+    exports com.example.geekbrainscloudstorage;
+}
